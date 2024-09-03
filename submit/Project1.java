a185fc55dc9817adc8f95d41815f2e4bc48ad67c
Tyl0rTheCr3at0r <tsthomas1106@gmail.com>
2024-09-02 22:15:57 -0400

    First commit of blank project

---
+++ submit/Project1.java
@@ -0,0 +1,17 @@
+import java.util.AbstractList;
+import java.util.function.*;
+import java.util.ArrayList;
+/**
+Project1 - class that take a collection (AbstractList) as 
+argument and provides various operations on the content of that
+collection.
+
+@author  YOUR NAME
+@version DATE
+*/
+public class Project1<E extends Comparable<E>> 
+    implements CollectionUtilities<E>
+{
+   
+    // Your code goes here
+}
---
+++ submit/Project1Test.java
@@ -0,0 +1,50 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.function.*;
+
+
+public class Project1Test 
+{
+
+    /**
+     * runner - known as the "test runner" object, this is the
+     * object that you will use in testing. You will use this
+     * object in each of your tests after reinitializing (new)
+     * in the setup() method below.
+     * 
+     * For example, if you want to test it with Strings,
+     * create an instance like this:
+     * 
+     *  private CollectionUtilities<String> runner;
+     */
+
+    /**
+     * setup() method, runs before each test method below.
+     * Use this method to recreate the objects needed for
+     * testing your class.
+     */
+    @Before
+    public void setup()
+    {
+        // create some data for testing, then
+        // initialize your runner object.
+        //
+        // runner = new Project1<String>( pass some data );
+    }
+
+    /**
+     * testingOne() 
+     */
+    @Test
+    public void testingOne()
+    {
+        // do testing here
+    }
+
+    // Add more tests here. Remember, the goal is to
+    // test every method and to execute every line of code
+    // in your Project1 class.
+}
